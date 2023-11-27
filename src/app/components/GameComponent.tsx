7789bb3da0fe86678abfecffe7fc8aa49576a406
Bryan Valeriano <bryan.valeriano.ext@Bryans-MacBook-Pro-842.local>
2023-11-27 17:53:23 -0300

    add game component

---
+++ src/app/components/GameComponent.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { useState } from 'react';
+
+interface GameProps {
+  title: string;
+  description: string;
+  gameUrl: string;
+}
+
+export default function GameComponent({ title, description, gameUrl }: GameProps) {
+  const [iframeSrc, setIframeSrc] = useState(gameUrl);
+
+  const refreshGame = () => {
+    setIframeSrc(''); // Set src to empty to trigger unload
+    setTimeout(() => setIframeSrc(gameUrl), 10); // Then set it back to gameUrl
+  };
+
+  return (
+    <div className="border p-4 rounded-lg shadow-lg">
+      <h2 className="text-lg font-bold">{title}</h2>
+      <p>{description}</p>
+      <div className="mt-2">
+        <iframe
+          src={iframeSrc}
+          width="100%"
+          height="400px"
+          frameBorder="0"
+          allowFullScreen>
+        </iframe>
+        <button onClick={refreshGame} className="mt-2">
+          Refresh Game
+        </button>
+      </div>
+    </div>
+  );
+};
