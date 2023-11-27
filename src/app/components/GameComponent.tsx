c575dfa083bc2706fbb754d8c6c091e1fb56e74a
Bryan Valeriano <bryan.valeriano.ext@Bryans-MacBook-Pro-842.local>
2023-11-27 19:41:22 -0300

    update refresh button

--- src/app/components/GameComponent.tsx
+++ src/app/components/GameComponent.tsx
@@ -28,7 +28,7 @@ export default function GameComponent({ title, description, gameUrl }: GameProps
           frameBorder="0"
           allowFullScreen>
         </iframe>
-        <button onClick={refreshGame} className="mt-2">
+        <button onClick={refreshGame} className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
           Refresh Game
         </button>
       </div>
