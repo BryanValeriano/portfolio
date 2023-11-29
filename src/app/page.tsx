3491d951aa6e898bf532766a8eb8cef5796f4dd9
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-11-29 00:46:43 -0300

    add api call example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
 import GamePreview from './components/GamePreview';
 
-export default function Home() {
+async function getData() {
+  const res = await fetch(process.env.NEXT_PUBLIC_URL + '/api/gamesdetails', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    cache: 'no-store'
+  });
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <GamePreview
