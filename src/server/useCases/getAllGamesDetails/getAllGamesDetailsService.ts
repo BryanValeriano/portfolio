40cf2e766f7b53c268b337a63784b20789c8927d
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-11-28 23:15:12 -0300

    add get all games details use case

--- src/server/useCases/getAllGamesDetails/getAllGamesDetailsService.ts
+++ src/server/useCases/getAllGamesDetails/getAllGamesDetailsService.ts
@@ -1,5 +1,18 @@
-import { IGamesDetailsRepository } from '@/server/repositories/IGamesDetailsRepository';
+import { GameDetail, IGamesDetailsRepository } from '@/server/repositories/IGamesDetailsRepository';
 
 type Dependencies = {
   gamesDetailsRepository: IGamesDetailsRepository;
-};
+}
+
+export default class GetAllGamesDetailsService {
+  private gamesDetailsRepository: IGamesDetailsRepository;
+
+  constructor({ gamesDetailsRepository }: Dependencies) {
+    this.gamesDetailsRepository = gamesDetailsRepository;
+  }
+
+  public execute(): GameDetail[] {
+    const gamesDetails = this.gamesDetailsRepository.getAll();
+    return gamesDetails;
+  }
+}
