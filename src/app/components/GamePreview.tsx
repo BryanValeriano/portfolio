8afb977523db0e5ec669bff2f64e6eba7f402f50
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-12-03 00:23:35 -0300

    update GamePreview styling

--- src/app/components/GamePreview.tsx
+++ src/app/components/GamePreview.tsx
@@ -9,11 +9,20 @@ interface GamePreviewProps {
 
 export default function GamePreview({ gameId, title, previewImageUrl }: GamePreviewProps) {
   return (
-    <div className="max-w-sm rounded overflow-hidden border border-gray-600 hover:border-gray-400 shadow-lg hover:shadow-2xl transition duration-300 ease-in-out bg-gray-800">
+    <div className="max-w-sm rounded-lg overflow-hidden shadow-lg transition duration-300 ease-in-out bg-gray-500 relative">
+      {/* Background Image */}
+      <div className="absolute inset-0 bg-cover bg-center opacity-75" style={{ backgroundImage: `url(${previewImageUrl})` }}></div>
+
+      {/* Blur Overlay */}
+      <div className="absolute inset-0 backdrop-blur-xl"></div> {/* Adjust blur intensity as needed */}
+
+      {/* Content */}
       <Link href={`/games/${gameId}`}>
-        <img className="w-full" src={previewImageUrl} alt={title} />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2 text-white">Play</div>
+        <div className="relative z-10"> {/* Ensure this content is above the overlay */}
+          <img className="w-full h-48 object-cover rounded-xl p-2" src={previewImageUrl} alt={title} />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2 text-white">{title}</div>
+          </div>
         </div>
       </Link>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
   const gamesDetails: GameDetail[] = data.gamesDetails;
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
       {gamesDetails.map((game) => (
         <GamePreview
           key={game.id}
@@ -29,6 +29,6 @@ export default async function Home() {
           previewImageUrl={game.previewImageUrl}
         />
       ))}
-    </main>
+    </div>
   );
 }
