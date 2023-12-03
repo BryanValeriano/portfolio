e812ee05b72b3288db239b5d7e64921a5128c3ef
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-12-03 06:20:12 -0300

    update game preview style

--- src/app/components/GamePreview.tsx
+++ src/app/components/GamePreview.tsx
@@ -4,27 +4,36 @@ import Link from 'next/link';
 interface GamePreviewProps {
   gameId: string;
   title: string;
+  description: string;
   previewImageUrl: string;
 }
 
-export default function GamePreview({ gameId, title, previewImageUrl }: GamePreviewProps) {
+export default function GamePreview({ gameId, title, description, previewImageUrl }: GamePreviewProps) {
   return (
-    <div className="max-w-sm rounded-lg overflow-hidden shadow-lg transition duration-300 ease-in-out bg-gray-500 relative">
+    <div className="max-w-sm rounded-xl overflow-hidden shadow-lg transition duration-300 ease-in-out bg-gray-500 relative">
       {/* Background Image */}
-      <div className="absolute inset-0 bg-cover bg-center opacity-75" style={{ backgroundImage: `url(${previewImageUrl})` }}></div>
+      <div className="absolute inset-0 bg-cover bg-center opacity-70" style={{ backgroundImage: `url(${previewImageUrl})` }}></div>
 
       {/* Blur Overlay */}
-      <div className="absolute inset-0 backdrop-blur-xl"></div> {/* Adjust blur intensity as needed */}
+      <div className="absolute inset-0 backdrop-blur-3xl"></div> {/* Adjust blur intensity as needed */}
 
       {/* Content */}
       <Link href={`/games/${gameId}`}>
-        <div className="relative z-10"> {/* Ensure this content is above the overlay */}
-          <img className="w-full h-48 object-cover rounded-xl p-2" src={previewImageUrl} alt={title} />
-          <div className="px-6 py-4">
+        <div className="relative z-10 flex flex-col justify-between h-full"> {/* Ensure this content is above the overlay */}
+          <img className="w-full h-52 object-cover rounded-xl p-2" src={previewImageUrl} alt={title} />
+          <div className="px-4">
             <div className="font-bold text-xl mb-2 text-white">{title}</div>
+            <p className="text-white text-base">{description}</p>
+          </div>
+          {/* Footer with Play Button */}
+          <div className="p-2  text-center">
+            <button className="text-zinc-500 w-full bg-gray-100 hover:opacity-60 font-bold p-2 rounded-full transition duration-300 ease-in-out transform hover:scale-[1.01]">
+              Play
+            </button>
           </div>
         </div>
       </Link>
     </div>
   );
 }
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,12 +20,13 @@ export default async function Home() {
   const gamesDetails: GameDetail[] = data.gamesDetails;
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 gap-2">
       {gamesDetails.map((game) => (
         <GamePreview
           key={game.id}
           gameId={game.id}
           title={game.title}
+          description={game.description}
           previewImageUrl={game.previewImageUrl}
         />
       ))}
