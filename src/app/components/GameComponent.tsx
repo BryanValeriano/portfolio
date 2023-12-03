c23d7a954ac2ba2e82ffb4af67b41d824046fbd1
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-12-03 06:52:25 -0300

    update game component style

--- src/app/components/GameComponent.tsx
+++ src/app/components/GameComponent.tsx
@@ -17,21 +17,37 @@ export default function GameComponent({ title, description, gameUrl }: GameProps
   };
 
   return (
-    <div className="border p-4 rounded-lg shadow-lg">
+    <div className="border p-4 rounded-lg shadow-lg flex flex-col items-center">
       <h2 className="text-lg font-bold">{title}</h2>
       <p>{description}</p>
-      <div className="mt-2">
-        <iframe
-          src={iframeSrc}
-          width="100%"
-          height="400px"
-          allowFullScreen
-          title="playable"
-        />
-        <button type="button" onClick={refreshGame} className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-          Play Again
-        </button>
+      <div className="mt-2 relative">
+        {/* Mobile frame as a background image */}
+        <div className="mobile-frame" style={{
+          backgroundImage: 'url(/iphone.png)',
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          width: '400px', // Adjust to the width of your mobile frame
+          height: '600px', // Adjust to the height of your mobile frame
+          position: 'relative'
+        }}>
+          {/* iframe positioned inside the mobile frame */}
+          <iframe
+            src={iframeSrc}
+            style={{
+              position: 'absolute',
+              top: '7%',  // Adjust based on your frame's dimensions
+              left: '19%',
+              width: '62%',  // Adjust based on your frame's dimensions
+              height: '82%', // Adjust based on your frame's dimensions
+            }}
+            allowFullScreen
+            title="playable"
+          />
+        </div>
       </div>
+      <button type="button" onClick={refreshGame} className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Play Again
+      </button>
     </div>
   );
 }
